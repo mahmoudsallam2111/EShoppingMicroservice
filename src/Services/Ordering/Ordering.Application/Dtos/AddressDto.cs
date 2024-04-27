236ece54cee044bc3988675d4a5f579fb6b006ae
Mahmoud Sallam <m.sallam@qoad.com>
2024-04-27 19:08:12 +0300

    Srart developping Application Layer of Order service

---
+++ src/Services/Ordering/Ordering.Application/Dtos/AddressDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Ordering.Application.Dtos;
+
+public record AddressDto(string firstName,
+    string LastName, 
+    string EmailAddress, 
+    string AddressLine,
+    string Country, 
+    string State, 
+    string ZipCode);
