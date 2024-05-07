8db079ff7d32d333f3944d9a710c93aa1e2f6d9a
Mahmoud Sallam <m.sallam@qoad.com>
2024-05-07 23:11:13 +0300

    Finish Developping Yarp Api Gateway

---
+++ src/ApiGateways/YarpApiGateway/Extensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.RateLimiting;
+
+namespace YarpApiGateway;
+
+public static class Extensions
+{
+    public static IServiceCollection RegisterServices(this IServiceCollection services , IConfiguration configuration)
+    {
+        services.AddReverseProxy()
+            .LoadFromConfig(configuration.GetSection("ReverseProxy"));
+
+        // if we exceed the limit of request per time , the response will be unavaliable
+        services.AddRateLimiter(opt =>
+        {
+            opt.AddFixedWindowLimiter("fixed", options =>
+            {
+                options.Window = TimeSpan.FromSeconds(10);
+                options.PermitLimit = 5;
+            });
+        });
+        return services;
+    }
+
+    public static WebApplication UseServices(this WebApplication app)
+    {
+        app.UseRateLimiter();  // this must be before mapreverseproxy pipeline
+
+        app.MapReverseProxy();
+        return app;
+    }
+}
--- src/ApiGateways/YarpApiGateway/Program.cs
+++ src/ApiGateways/YarpApiGateway/Program.cs
@@ -1,5 +1,12 @@
+using YarpApiGateway;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services
+    .RegisterServices(builder.Configuration);
+
 var app = builder.Build();
+
+app.UseServices();
 
 app.Run();
